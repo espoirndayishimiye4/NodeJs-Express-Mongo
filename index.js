e12eb01529634fa06eec77f91db1b2946352e6f5
espoirndayishimiye4 <espoirndayishimiye4@gmail.com>
2022-06-14 19:29:02 -0700

    jwt

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const verifyJWT = require('./middleware/verifyJWT');
 const PORT = process.env.PORT || 3500;
 
 app.use(express.json());
 
-app.use('/employees',require('./routes/employees'));
+
 app.use('/register',require('./routes/register'));
 app.use('/auth',require('./routes/auth'));
+app.use(verifyJWT);
+app.use('/employees',require('./routes/employees'));
 
 app.get('/', (req,res) =>{
     res.send("hello world");
