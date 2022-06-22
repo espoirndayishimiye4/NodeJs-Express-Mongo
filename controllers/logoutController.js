84b85c894f86f99cd7ff17dc0c5cafc720d609d7
espoirndayishimiye4 <espoirndayishimiye4@gmail.com>
2022-06-21 18:25:02 -0700

    commit

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -6,7 +6,7 @@ const userDB ={
 const fspromises = require('fs').promises;
 const path = require('path');
 
-const handleRefreshToken = (req,res) =>{
+const handleLogout = async (req,res) =>{
 
     const cookies = req.cookies;
     if(!cookies?.jwt) return res.sendStatus(204);
@@ -17,21 +17,16 @@ if(!finduser){
     res.clearCookie('jwt',{httpOnly:true})
     return res.status(204);
 } 
-
-jwt.verify(
-    jwt.refreshToken,
-    process.env.REFRESH_TOKEN_SECRET,
-    (err, decoded) => {
-        if(err || finduser.username !== decoded.username) return res.sendStatus(403);
-        const accessToken = jwt.sign(
-            {"username": decoded.username},
-            process.env.REFRESH_TOKEN_SECRET,
-            {expiresIn: '30s'}
-        )
-        res.json({accessToken});
-    }
-)
-
+//delete refreshToken
+const otherUsers = userDB.users.filter(person => person.refreshToken !== finduser.refreshToken);
+const currentUser ={...finduser, refreshToken: ''};
+userDB.setUsers([...otherUsers,currentUser]);
+await fspromise.writeFile(
+    path.join(__dirname,'..','model','users.json'),
+    JSON.stringify(userDB.users)
+);
+res.clearCookie('jwt',{httpOnly: true});
+res.sendStatus(204);
 };
 
-module.exports = {handleRefreshToken};
+module.exports = {handleLogout};
--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3500;
 
 app.use(express.json());
-app.use(cookieParser);
+
 
 
 app.use('/register',require('./routes/register'));
 app.use('/auth',require('./routes/auth'));
 app.use('/refresh',require('./routes/refresh'));
+app.use('/logout',require('./routes/logout'));
 app.use(verifyJWT);
 app.use('/employees',require('./routes/employees'));
 
